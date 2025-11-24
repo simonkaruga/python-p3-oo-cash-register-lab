32bd0bdaeefc7cd8a4c611a61ec3ef45682977aa
simonkaruga <simonkaruganjoroge@gmail.com>
2025-11-24 15:19:23 +0300

    Add origin

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    self.transactions = []
+
+  def add_item(self, title, price, quantity=1):
+    self.transactions.append((title, price, quantity))
+    self.total += price * quantity
+    for _ in range(quantity):
+      self.items.append(title)
+  def apply_discount(self):
+    if self.discount > 0:
+      discount_amount = (self.discount / 100) * self.total
+      self.total -= discount_amount
+      print(f"After the discount, the total comes to ${int(self.total)}.")
+    else:
+      print("There is no discount to apply.")
+
+
+  def void_last_transaction(self):
+    if self.transactions:
+      last_trans = self.transactions.pop()
+      title, price, qty = last_trans
+      self.total -= price * qty
+      self.items = self.items[:-qty]
+    else:
+      self.total = 0
